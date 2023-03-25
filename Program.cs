93df857b756dc4fd6eeead5d5eee3894fa766dba
shash <shash@HANKUCHIM>
2023-03-25 11:29:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace RationalNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            RationalNumber a = new RationalNumber(1, 2);
+            RationalNumber b = new RationalNumber(3, 4);
+
+            RationalNumber sum = a + b;
+            Console.WriteLine("Сумма: {0}", sum);
+
+            RationalNumber difference = a - b;
+            Console.WriteLine("Разность: {0}", difference);
+
+            RationalNumber product = a * b;
+            Console.WriteLine("Произведение: {0}", product);
+
+            RationalNumber quotient = a / b;
+            Console.WriteLine("Частное: {0}", quotient);
+
+            bool greater = a > b;
+            Console.WriteLine("{0} > {1}: {2}", a, b, greater);
+
+            bool equal = a == b;
+            Console.WriteLine("{0} == {1}: {2}", a, b, equal);
+
+            Console.WriteLine(a.Info()); // RationalNumber(1/2)
+        }
+    }
+}
---
+++ RationalNumber.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalNumbers
+{
+    using System;
+
+    public class RationalNumber
+    {
+        private int numerator;
+        private int denominator;
+
+        // Конструктор класса RationalNumber
+        public RationalNumber(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new ArgumentException("Знаменатель не может быть равен нулю");
+
+            int gcd = GreatestCommonDivisor(numerator, denominator);
+            this.numerator = numerator / gcd;
+            this.denominator = denominator / gcd;
+
+            if (this.denominator < 0)
+            {
+                this.numerator = -this.numerator;
+                this.denominator = -this.denominator;
+            }
+        }
+
+        // Методы класса RationalNumber
+
+        // Сложение
+        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        {
+            int numerator = r1.numerator * r2.denominator + r2.numerator * r1.denominator;
+            int denominator = r1.denominator * r2.denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        // Вычитание
+        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+        {
+            int numerator = r1.numerator * r2.denominator - r2.numerator * r1.denominator;
+            int denominator = r1.denominator * r2.denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        // Умножение
+        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+        {
+            int numerator = r1.numerator * r2.numerator;
+            int denominator = r1.denominator * r2.denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        // Деление
+        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+        {
+            int numerator = r1.numerator * r2.denominator;
+            int denominator = r1.denominator * r2.numerator;
+            return new RationalNumber(numerator, denominator);
+        }
+
+        // Операции сравнения
+        public static bool operator ==(RationalNumber r1, RationalNumber r2)
+        {
+            if (ReferenceEquals(r1, r2))
+            {
+                return true;
+            }
+
+            if ((object)r1 == null || (object)r2 == null)
+            {
+                return false;
+            }
+
+            return r1.numerator == r2.numerator && r1.denominator == r2.denominator;
+        }
+
+        public static bool operator !=(RationalNumber r1, RationalNumber r2)
+        {
+            return !(r1 == r2);
+        }
+
+        public static bool operator <(RationalNumber r1, RationalNumber r2)
+        {
+            int numerator1 = r1.numerator * r2.denominator;
+            int numerator2 = r2.numerator * r1.denominator;
+            return numerator1 < numerator2;
+        }
+
+        public static bool operator >(RationalNumber r1, RationalNumber r2)
+        {
+            return r2 < r1;
+        }
+
+        public static bool operator <=(RationalNumber r1, RationalNumber r2)
+        {
+            return r1 < r2 || r1 == r2;
+        }
+
+        public static bool operator >=(RationalNumber r1, RationalNumber r2)
+        {
+            return r1 > r2 || r1 == r2;
+        }
+        public override string ToString()
+        {
+            if (denominator == 1)
+                return numerator.ToString();
+            else
+                return string.Format("{0}/{1}", numerator, denominator);
+        }
+
+        public string Info()
+        {
+            return string.Format("RationalNumber({0})", this);
+        }
+
+        // Вспомогательный метод для нахождения наибольшего общего делителя
+        private int GreatestCommonDivisor(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (b != 0)
+            {
+                int remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            return a;
+        }
+    }
+    
+}
